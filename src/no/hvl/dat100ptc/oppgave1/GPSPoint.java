fe5160e3c42c50177c13b562463095c2691faeb2
geirsatre <geirsatre@localhost>
2022-10-07 13:27:56 +0200

    1

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,75 +4,85 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 
+	private int time = 0;
+	private double latitude = 0;
+	private double longitude = 0;
+	private double elevation = 0;
+
 	// TODO - objektvariable
-		
+
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
-		// TODO - konstruktør
-
-		// Fjern linjene med throw new etterhvert som metodene implementeres
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
+		this.time = time;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.elevation = elevation;
 
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return time;
+
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
+
+		this.time = time;
+
+		// throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return latitude;
+
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.latitude = latitude;
+
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return longitude;
+
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.longitude = longitude;
+
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return elevation;
+
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.elevation = elevation;
+
 	}
-	
+
 	public String toString() {
-		
+
 		String str;
-		
+
+		str = getTime() + " (" + getLatitude() + "," + getLongitude() + ") " + getElevation() + "\n";
+		return str;
+
 		// TODO - start
 
-		throw new UnsupportedOperationException(TODO.method());
+		// throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - slutt
-		
+
 	}
 }
